fb4996fc64f410fb013d1a53e7334ccdab4d89aa
abanderov <a_banderov@yahoo.com>
2017-09-26 10:11:36 +0300

    homework adv syntax

--- 04-PHP_Adv_Syntax_SoftUni_09_2017/07-ExamShopping.php
+++ 04-PHP_Adv_Syntax_SoftUni_09_2017/07-ExamShopping.php
@@ -6,18 +6,58 @@
  * Time: 12:34
  */
 
-$shopInventory = [];
+$shoppingInv = [];
 while($input = trim(fgets(STDIN))) {
+    if($input == 'shopping time'){ continue;}
+    if($input == 'exam time'){break;}
     // do stock
-
     $data = explode(' ', $input);
-    $shopInventory[$data[1]] = $data[2];
-
-    if($input == 'shopping time'){
-       break; // do buy (stock - buy quant)
-    }elseif($input == 'exam time'){
-        break;
+    $product = $data[1];
+    $quantity = intval($data[2]);
+    if ($data[0] == 'stock') {
+        if (!isset($shoppingInv[$product])) {
+            $shoppingInv[$product]= $quantity;
+        } else {
+            $shoppingInv[$product] += $quantity;
+        }
+    }elseif ($data[0] == 'buy'){
+        if (!isset($shoppingInv[$product])){
+            echo "$product does not exist \n";
+        }elseif(isset($shoppingInv[$product])){
+            $stock = intval($shoppingInv[$product]);
+            if($stock == 0){
+                echo "$product out of stock \n";
+                break;
+            }elseif($quantity > $stock){
+                $shoppingInv[$product] = 0;
+            }else{
+                $shoppingInv[$product] -= $quantity;
+            }
+        }
     }
 }
-array_count_values($shopInventory);
-print_r($shopInventory);
+foreach($shoppingInv as $item => $value) {
+    if ($value == 0) {
+        continue;
+    }
+    echo "$item -> $value \n";
+}
+
+
+
+
+
+
+
+
+
+//ksort($shopInventory);
+////array_count_values($shopInventory);
+//print_r($result);
+//    If (!array_key_exists($data[1],$result)){
+//        $result[$shopInventory[$data[1]]] += $data[2];
+//    }
+//    $result[$shopInventory[$data[1]]] = $data[2];
+//
+//
+//    //print_r($shopInventory);
